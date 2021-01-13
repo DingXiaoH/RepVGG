022f7e49bc063dce06ddb3d6823358aa7be80789
dingxiaohan <dingxiaohan@megvii.com>
2021-01-13 15:22:23 +0800

    Update repvgg.py

--- repvgg.py
+++ repvgg.py
@@ -53,7 +53,7 @@ class RepVGGBlock(nn.Module):
 #   You can get the equivalent kernel and bias at any time and do whatever you want,
     #   for example, apply some penalties or constraints during training, just like you do to the other models.
 #   May be useful for quantization or pruning.
-    def get_equivalent_kernel(self):
+    def get_equivalent_kernel_bias(self):
         kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
         kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
         kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
@@ -96,7 +96,7 @@ class RepVGGBlock(nn.Module):
         return kernel * t, beta - running_mean * gamma / std
 
     def repvgg_convert(self):
-        kernel, bias = self.get_equivalent_kernel()
+        kernel, bias = self.get_equivalent_kernel_bias()
         return kernel.detach().cpu().numpy(), bias.detach().cpu().numpy(),
 
     # def _fuse_bn(self, branch):
