60c6de320839266e89f29f8195d07c8cdb377c8d
gaohaibin <gaohaibin@wisview.com>
2021-06-28 13:38:58 +0800

    simplify reshape with torch.flatten

--- repvgg.py
+++ repvgg.py
@@ -181,7 +181,7 @@ class RepVGG(nn.Module):
         out = self.stage3(out)
         out = self.stage4(out)
         out = self.gap(out)
-        out = out.view(out.size(0), -1)
+        out = torch.flatten(out, 1)
         out = self.linear(out)
         return out
 
